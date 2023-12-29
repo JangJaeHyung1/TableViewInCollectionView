9b806b41a10ae2fd8ee1b68c05c653f4f672d3cf
jangjaehyung1 <drezzed7@gmail.com>
2023-12-29 10:45:51 +0900

    Initial Commit

---
+++ SideTemplateCVInTV/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SideTemplateCVInTV
+//
+//  Created by jh on 2023/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
